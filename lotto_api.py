eb1d7576232b0a015412b15c41fbad249e2cc5c9
kyle12223 <kasongok240@gmail.com>
2025-07-10 23:37:06 +0200

    Update lotto_api.py

--- lotto_api.py
+++ lotto_api.py
@@ -25,4 +25,6 @@ def lotto_data():
     return jsonify(draws)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+   import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
